ce7997d85e1a65cc794e0ca1517efebfbff9a127
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2020-01-08 10:23:35 -0500

    nobody likes dark backgounds, added svgs

--- scripts.js
+++ scripts.js
@@ -1,16 +1,38 @@
 //make labels do the thing
+//-----------------------------------------------------------------
 function labels(){
 const signInput = document.querySelectorAll('.signInput');
 signInput.forEach(elem => {
 	const inputField = elem.querySelector('input');
 	const inputClass = elem.classList;
-	if (elem.querySelector("input").value)
-		inputClass.add("active");
-		inputField.addEventListener('focus', () => {
+	if (elem.querySelector("input").value){inputClass.add("active");}
+	inputField.addEventListener('focus', () => {
 		inputClass.add('active');});
 	inputField.addEventListener('blur', () => {
 		if(!inputField.value){inputClass.remove('active');}});
 })}
+//-----------------------------------------------------------------
+
+
+//password stuff
+//-----------------------------------------------------------------
+function password(){
+	//password
+	var password = document.getElementById("pass");
+	password.addEventListener('focus', () => {
+		document.getElementById("diffic").classList = ('diffic active');});
+	password.addEventListener('blur', () => {
+			if(password.value.length == 0){document.getElementById("diffic").classList = ('diffic');}})
+	document.getElementById("pass").addEventListener("input", function(){difficulty(password.value);});
+	//document.getElementById("pass").onkeyup = feedback();
+
+	//confirm password
+	var cpassword = document.getElementById("passc");
+	cpassword.addEventListener('focus', () => {
+		cpassword.classList.add(matching(password.value, cpassword.value))});
+	cpassword.addEventListener('blur', () => {
+		cpassword.classList.remove(matching(password.value, cpassword.value));});
+}
 
 //realtime feedback stuff
 function feedback(password){
@@ -34,28 +56,52 @@ function feedback(password){
 		return("no unicode please, it frightens the programmers");
 }}
 
-//realtime diffuclty bar
-function difficulty(){
+function difficulty(password){
 	var dbar = document.getElementById("dbar");
-	var password = document.getElementById("pass").value;
-	var diff = password.length;
+	var leng = password.length;
 
 	var req = document.getElementById("req");
 	req.innerHTML = (feedback(password));
 
-	if(diff >= 20){
+	if(leng >= 18){
 		dbar.classList = ('dbar excellent');
-	}else if(diff >= 15){
+	}else if(leng >= 12){
 		dbar.classList = ('dbar good');
-	}else if(diff >= 10){
+	}else if(leng >= 6){
 		dbar.classList = ('dbar easy');
-	}else if(diff >= 5){
+	}else if(leng >= 1){
 		dbar.classList = ('dbar trivial');
 	}else {dbar.classList = ('dbar');}
 }
 
+function matching(password, cpassword){
+	if(password == cpassword){
+		return "good"
+	}else{
+		return "bad"
+	}
+}
+//-----------------------------------------------------------------
+
+//email formatting
+//-----------------------------------------------------------------
+function phone(){
+	var email = document.getElementById("phone");
+	if (email.value){inputClass.add("active");}
+	inputField.addEventListener('focus', () => {
+		inputClass.add('active');});
+	inputField.addEventListener('blur', () => {
+		inputClass.remove('active');});
+}
+
+function phone_formatting(){
+	phone.value="(___) ___-____";
+	//phone.data-mask = "(___) ___-____";
+}
+//-----------------------------------------------------------------
+
 window.onload = function() {
 	labels();
-	//document.getElementById("pass").onkeyup = feedback();
-	document.getElementById("pass").addEventListener("keyup", function(){difficulty();});
+	password();
+	phone();
 }
